6f8cd7a7ce6ad400d04cf6483e9618a13bfbc180
Adhithyaa Senthil <146030973+adhisenthil22@users.noreply.github.com>
2024-03-20 17:01:35 -0500

    Initial Commit

---
+++ LYNKmodel1/LYNKmodel1App.swift
@@ -0,0 +1,17 @@
+//
+//  LYNKmodel1App.swift
+//  LYNKmodel1
+//
+//  Created by Adhithyaa Senthil on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LYNKmodel1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
